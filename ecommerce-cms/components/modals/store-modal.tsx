0daa12039e0be182ba7d10b4ac7d4c757ad25384
maikolcw <whysoraccoony@gmail.com>
2023-09-18 19:44:58 -0700

    updated store modal with form

--- ecommerce-cms/components/modals/store-modal.tsx
+++ ecommerce-cms/components/modals/store-modal.tsx
@@ -1,13 +1,36 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+
 import { Modal } from "@/components/ui/modal";
-
-
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
+
+// at least one character is needed
+const formSchema = z.object({
+    name: z.string().min(1),
+});
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    };
 
     return (
         <Modal
@@ -16,7 +39,35 @@ export const StoreModal = () => {
             isOpen={storeModal.isOpen}
             onClose={storeModal.onClose}
         >
-            Hello Wurld
+            <div>
+                <div className="space-y-4 py-2 pb-4">
+                    <div className="space-y-2">
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)}>
+                                <FormField
+                                    control={form.control}
+                                    name="name"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Name</FormLabel>
+                                            <FormControl>
+                                                <Input disabled={loading} placeholder="E-Commerce" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                    <Button disabled={loading} variant="outline" onClick={storeModal.onClose}>
+                                        Cancel
+                                    </Button>
+                                    <Button disabled={loading} type="submit">Continue</Button>
+                                </div>
+                            </form>
+                        </Form>
+                    </div>
+                </div>
+            </div>
         </Modal>
     );
 };
