98fb81a36118c76b6c645be634906c0203399385
maikolcw <whysoraccoony@gmail.com>
2023-09-18 17:54:55 -0700

    added modal component and updated root page

--- ecommerce-cms/app/(root)/page.tsx
+++ ecommerce-cms/app/(root)/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button";
+"use client"
+
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 
 const SetupPage = () => {
     return (
         <div className="p-4">
-            <UserButton afterSignOutUrl="/" />
+            <Modal title="Test" description="Test description" isOpen onClose={() => {}}></Modal>
         </div>
     )
 }
