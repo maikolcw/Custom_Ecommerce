6a4a45c0a88c4d090b791cca88bd5190bbcc3f62
maikolcw <whysoraccoony@gmail.com>
2023-09-18 18:20:12 -0700

    updated documentation for modal provider and updated root layout

--- ecommerce-cms/app/layout.tsx
+++ ecommerce-cms/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ModalProvider } from '@/providers/modal-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <ModalProvider />
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
--- ecommerce-cms/providers/modal-provider.tsx
+++ ecommerce-cms/providers/modal-provider.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 import { StoreModal } from "@/components/modals/store-modal";
 
-// helper to use StoreModal everywhere
+// helper to use StoreModal everywhere, circumvent hydration errors with client and server
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
 
@@ -12,10 +12,12 @@ export const ModalProvider = () => {
         setIsMounted(true);
     }, []);
 
+    // if on server side
     if (!isMounted) {
         return null;
     }
 
+    // else on client side
     return (
         <>
             <StoreModal />
