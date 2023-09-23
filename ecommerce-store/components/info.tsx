3293446cd7d32e561d73e735071297a31f3b6153
maikolcw <whysoraccoony@gmail.com>
2023-09-22 21:28:56 -0700

    fixed error with p tag inside div and updated add cart function to info

--- ecommerce-store/components/info.tsx
+++ ecommerce-store/components/info.tsx
@@ -5,20 +5,26 @@ import { ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { Product } from "@/types";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
     data: Product
 };
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+    const cart = useCart();
+
+    const onAddToCart = () => {
+        cart.addItem(data);
+    }
 
     return (
         <div>
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
             <div className="mt-3 flex items-end justify-between">
-                <p className="text-2xl text-gray-900">
+                <div className="text-2xl text-gray-900">
                     <Currency value={data?.price} />
-                </p>
+                </div>
             </div>
             <hr className="my-4" />
             <div className="flex flex-col gap-y-6">
@@ -34,7 +40,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
                 </div>
             </div>
             <div className="mt-10 flex items-center gap-x-3">
-                <Button onClick={() => {}} className="flex items-center gap-x-2">
+                <Button onClick={onAddToCart} className="flex items-center gap-x-2">
                     Add To Cart
                     <ShoppingCart size={20} />
                 </Button>
