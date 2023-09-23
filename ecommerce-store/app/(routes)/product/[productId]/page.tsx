32b3a24ae3f4f45317f1a3eaef26d3945ddc37cb
maikolcw <whysoraccoony@gmail.com>
2023-09-22 18:38:24 -0700

    added info component

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,6 @@
 import ProductList from '@/components/product-list'
 import Gallery from '@/components/gallery';
+import Info from '@/components/info';
 import getProduct from '@/actions/get-product';
 import getProducts from '@/actions/get-products';
 import Container from '@/components/ui/container';
@@ -31,6 +32,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({
                     <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
                         <Gallery images={product.images} />
                         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+                            <Info data={product} />
                         </div>
                     </div>
                     <hr className="my-10" />
